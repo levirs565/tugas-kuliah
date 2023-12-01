8042daff083cb000ed089f483399a68f31e51905
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2023-12-01 21:12:57 +0700

    add hex conversion

--- StrKeAngka.cpp
+++ StrKeAngka.cpp
@@ -4,6 +4,67 @@
 
 using namespace std;
 
+string intKeStr(int angka);
+
+int hexCharToInt(char hex)
+{
+    if (hex >= 'a' && hex <= 'f')
+        return 10 + (hex - 'a');
+    if (hex >= '0' && hex <= '9')
+        return hex - '0';
+    if (hex >= 'A' && hex <= 'F')
+        return 10 + (hex - 'A');
+    throw invalid_argument(string("karakter bukanlah bilangan hex ") + hex);
+}
+
+int hexStrToInt(string hex)
+{
+    int angka = 0;
+    int factor = 1;
+    for (int i = hex.length() - 1; i >= 0; i--)
+    {
+        char currentChar = hex.at(i);
+        int currentNumber = hexCharToInt(currentChar);
+        angka += currentNumber * factor;
+        factor *= 16;
+    }
+    return angka;
+}
+
+char intToHexChar(int angka)
+{
+    if (angka >= 0 && angka <= 9)
+        return angka + '0';
+    if (angka <= 15)
+        return 'a' + (angka - 10);
+    throw invalid_argument(string("karakter melambangkan hex lebih dari 1 digit: ") + intKeStr(angka));
+}
+
+string intToHexStr(int angka)
+{
+    int digitCount = (int)floor(log(angka) / log(16)) + 1;
+    char str[digitCount + 1] = {};
+    str[digitCount] = '\0';
+
+    int currenDivider = 1;
+
+    for (int power = 1; power < digitCount; power++)
+    {
+        currenDivider *= 16;
+    }
+
+    int tersisa = angka;
+    for (int i = 0; i < digitCount; i++)
+    {
+        int currentDigit = (int)floor(tersisa / currenDivider);
+        str[i] = intToHexChar(currentDigit);
+        tersisa = tersisa - currenDivider * currentDigit;
+        currenDivider /= 16;
+    }
+
+    return string(str);
+}
+
 char intKeChar(int angka)
 {
     if (angka < 0 || angka > 9)
@@ -27,7 +88,6 @@ string intKeStr(int angka)
     int tersisa = angka;
     for (int i = 0; i < digitCount; i++)
     {
-        const int powerFactor = digitCount - i;
         int currentDigit = (int)floor(tersisa / currentDivider);
         str[i] = intKeChar(currentDigit);
 
@@ -57,13 +117,24 @@ int strKeInt(string str)
 
 int main()
 {
+    cout << "Pilihan mode: " << endl;
+    cout << "1. Desimal (Basis 10) " << endl;
+    cout << "2. Hex (Basis 10) " << endl;
+    cout << "Pilih mode: ";
+
+    int mode;
+    cin >> mode;
+    cin.ignore();
+
     cout << "Masukkan angka\t: ";
     string str;
     getline(cin, str);
 
-    int hasil = strKeInt(str);
+    int hasil = mode == 1 ? strKeInt(str) : mode == 2 ? hexStrToInt(str)
+                                                      : 0;
     cout << "Angka hasil\t: " << hasil << endl;
 
-    string ubahKemabli = intKeStr(hasil);
+    string ubahKemabli = mode == 1 ? intKeStr(hasil) : mode == 2 ? intToHexStr(hasil)
+                                                                 : 0;
     cout << "String kembali\t: " << ubahKemabli << endl;
 }
